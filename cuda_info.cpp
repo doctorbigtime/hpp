fb7ab99e50bcc990ae7750e30622ede488569688
Sebastien Fortas <sebastien@fortas.org>
2017-07-23 16:45:55 -0400

    better cuda info

--- cuda_info.cpp
+++ cuda_info.cpp
@@ -2,47 +2,111 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-//@@ The purpose of this code is to become familiar with the submission 
-//@@ process. Do not worry if you do not understand all the details of 
-//@@ the code.
+enum class CudaArch
+{
+    Unknown,
+    Fermi = 2,
+    Kepler = 3,
+    Maxwell = 5,
+    Pascal = 6
+};
 
-int main(int argc, char ** argv) {
+
+std::ostream& operator<<(std::ostream& os, CudaArch const& a)
+{
+    switch(a)
+    {
+        case CudaArch::Fermi:
+            os << "Fermi";
+            break;
+        case CudaArch::Kepler:
+            os << "Kepler";
+            break;
+        case CudaArch::Maxwell:
+            os << "Maxwell";
+            break;
+        case CudaArch::Pascal:
+            os << "Pascal";
+            break;
+    }
+    return os;
+}
+
+CudaArch getArch(cudaDeviceProp const& prop)
+{
+    CudaArch arch;
+    switch(prop.major)
+    {
+        case 2: arch = CudaArch::Fermi;    break;
+        case 3: arch = CudaArch::Kepler;   break;
+        case 5: arch = CudaArch::Maxwell;  break;
+        case 6: arch = CudaArch::Pascal;   break;
+        default: arch = CudaArch::Unknown; break;
+    }
+    return arch;
+}
+
+int getCores(cudaDeviceProp const& prop)
+{
+    auto mp = prop.multiProcessorCount;
+    switch(getArch(prop))
+    {
+        case CudaArch::Fermi:
+            return mp * (prop.minor == 1 ? 32 : 48);
+        case CudaArch::Kepler:
+            return mp * 192;
+        case CudaArch::Maxwell:
+            return mp * 128;
+        case CudaArch::Pascal:
+            return mp * (prop.minor == 0 ? 64 : 128);
+        default:
+            std::cerr << "Unknown device type." << std::endl;
+    }
+    return 0;
+}
+
+auto main(int argc, char ** argv) -> int
+{
     int deviceCount;
-
-    //wbArg_read(argc, argv);
 
     cudaGetDeviceCount(&deviceCount);
 
-    //wbTime_start(GPU, "Getting GPU Data."); //@@ start a timer
-
-    for (int dev = 0; dev < deviceCount; dev++) {
+    for (int dev = 0; dev < deviceCount; dev++) 
+    {
         cudaDeviceProp deviceProp;
 
-        cudaGetDeviceProperties(&deviceProp, dev);
-
-        if (dev == 0) {
-            if (deviceProp.major == 9999 && deviceProp.minor == 9999) {
-                //wbLog(TRACE, "No CUDA GPU has been detected");
-                return -1;
-            } else if (deviceCount == 1) {
-                //@@ WbLog is a provided logging API (similar to Log4J).
-                //@@ The logging function wbLog takes a level which is either
-                //@@ OFF, FATAL, ERROR, WARN, INFO, DEBUG, or TRACE and a
-                //@@ message to be printed.
-                //wbLog(TRACE, "There is 1 device supporting CUDA");
-            } else {
-                //wbLog(TRACE, "There are ", deviceCount, " devices supporting CUDA");
-            }
+        auto error = cudaGetDeviceProperties(&deviceProp, dev);
+        if(error != cudaSuccess)
+        {
+            std::cerr << "Failed to get device properties for device #"
+                      << dev
+                      << std::endl;
+            return -1;
         }
 
-        std::cout << "Device " << dev << " name: " << deviceProp.name 
+        std::cout << "Device #" << dev << " name: " << deviceProp.name 
                   << "\n Computational Capabilities: " << deviceProp.major << '.' << deviceProp.minor
-                  << "\n Maximum global memory size: " << deviceProp.totalGlobalMem/(1024*1024) << " MB"
-                  << "\n Maximum constant memory size: " << deviceProp.totalConstMem
-                  << "\n Maximum shared memory size per block: " << deviceProp.sharedMemPerBlock
-                  << "\n Maximum block dimensions: " << deviceProp.maxThreadsDim[0] 
-                     << " x " << deviceProp.maxThreadsDim[1]
-                     << " x " << deviceProp.maxThreadsDim[2]
+                  << "\n Architecture              : " << getArch(deviceProp)
+                  << "\n Device clock rate         : " << deviceProp.clockRate/1000 << " MHz"
+                  << "\n Memory clock rate         : " << deviceProp.memoryClockRate/1000 << " MHz"
+                  << "\n Memory bus width          : " << deviceProp.memoryBusWidth << " bits"
+                  << "\n Peak memory bandwidth     : " << (((deviceProp.memoryClockRate*2.0)*(deviceProp.memoryBusWidth/8))/1e6) << " GB/s"
+                  << "\n Multiprocessor count      : " << deviceProp.multiProcessorCount
+                  << "\n CUDA cores                : " << getCores(deviceProp)
+                  << "\n Max global memory         : " << deviceProp.totalGlobalMem/(1024*1024) << " MB"
+                  << "\n Max constant memory       : " << deviceProp.totalConstMem
+                  << "\n Max shared memory/block   : " << deviceProp.sharedMemPerBlock
+                  << "\n Max block dimensions      : " << deviceProp.maxThreadsDim[0] 
+                                              << " x " << deviceProp.maxThreadsDim[1]
+                                              << " x " << deviceProp.maxThreadsDim[2]
+                  << "\n Max threads/block         : " << deviceProp.maxThreadsPerBlock
+                  << "\n Max threads/multiprocessor: " << deviceProp.maxThreadsPerMultiProcessor
+                  << "\n Warp size                 : " << deviceProp.warpSize
+                  << "\n Registers/block           : " << deviceProp.regsPerBlock
+                  << "\n Registers/multiprocessor  : " << deviceProp.regsPerMultiprocessor
+                  << "\n Max grid size             : " << deviceProp.maxGridSize[0]
+                                                << "," << deviceProp.maxGridSize[1]
+                                                << "," << deviceProp.maxGridSize[2]
                   << std::endl;
     }
     return 0;
